ed04ec4e7cae9a92bb299228a1fbe4912057a665
jahan-nusrat <njnusrat53@gmail.com>
2023-01-13 18:59:06 +0100

    reviews find for a productID

--- resolvers/Product.js
+++ resolvers/Product.js
@@ -1,8 +1,13 @@
-import { categories } from '../data.js';
+import { categories, reviews } from '../data.js';
 
 export const Product = {
   category: (parent, args, context) => {
     const { categoryId } = parent;
     return categories.find(category => category.id === categoryId);
+  },
+
+  review: (parent, args, context) => {
+    const { id: productId } = parent;
+    return reviews.filter(review => review.productId === productId);
   },
 };
