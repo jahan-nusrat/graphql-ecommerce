4902e1fa9d6ae959222cbb7acd6a6beaf2213e29
jahan-nusrat <njnusrat53@gmail.com>
2023-01-13 16:38:26 +0100

    products filter with category id

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const products = [
     price: 42.44,
     image: 'img-1',
     onSale: false,
+    categoryId: 'c01b1ff4-f894-4ef2-b27a-22aacc2fca70',
   },
   {
     id: 'c2af9adc-d0b8-4d44-871f-cef66f86f7f6',
@@ -19,9 +20,11 @@ const products = [
     price: 53.5,
     image: 'img-2',
     onSale: false,
+    categoryId: 'c01b1ff4-f894-4ef2-b27a-22aacc2fca70',
   },
   {
     id: '2c931e7e-510f-49e5-aed6-d6b44087e5a1',
+    categoryId: 'c01b1ff4-f894-4ef2-b27a-22aacc2fca70',
     name: 'Spoon',
     description: 'Small and delicate spoon',
     quantity: 4266,
@@ -137,6 +140,12 @@ const resolvers = {
     category: (parent, args, context) => {
       const { id: categoryId } = args;
       return categories.find(category => category.id === categoryId);
+    },
+  },
+  Category: {
+    products: (parent, args, context) => {
+      const { id: parentid } = parent;
+      return products.filter(product => product.categoryId === parentid);
     },
   },
 };
