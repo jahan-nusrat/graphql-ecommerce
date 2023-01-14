39fdf941f45272bf7cc5824649eb3e48a6444e6c
jahan-nusrat <njnusrat53@gmail.com>
2023-01-14 13:16:32 +0100

    filter products in category based on onSale arg

--- resolvers/Category.js
+++ resolvers/Category.js
@@ -2,7 +2,12 @@ import { products } from '../data.js';
 
 export const Category = {
   products: (parent, args, context) => {
+    const { filter } = args;
     const { id: parentid } = parent;
-    return products.filter(product => product.categoryId === parentid);
+    if (filter && filter.onSale) {
+      return products.filter(product => product.onSale);
+    } else {
+      return products.filter(product => product.categoryId === parentid);
+    }
   },
 };
